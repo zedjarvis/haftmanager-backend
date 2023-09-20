9832fbb62314f6bf5d4ff07168ad74cdc921c701
Cedrouseroll omondi <omondicedo@gmail.com>
2023-09-20 19:53:41 +0300

    feat[users]: send invitation and confirmation emails

---
+++ apps/users/email.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.tokens import default_token_generator
+from templated_mail.mail import BaseEmailMessage
+
+from djoser import utils
+from djoser.conf import settings
+
+INVITE_ACTIVATION_URL = "auth/invite/activate/{uid}/{token}"
+
+class InvitationEmail(BaseEmailMessage):
+    template_name = "email/invitation.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        user = context.get("user")
+        context["to_user"] = user.first_name
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = INVITE_ACTIVATION_URL.format(**context)
+
+        return context
+
+
+class ConfirmationEmail(BaseEmailMessage):
+    template_name = "email/confirmation.html"

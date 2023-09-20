b080cc8b639fb94fce2c9576b5500955cc0ac80d
Cedrouseroll omondi <omondicedo@gmail.com>
2023-09-20 19:56:00 +0300

    feat[users]: added invite user and confirm invitation api

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -1,13 +1,15 @@
-from rest_framework import serializers
+from django.db import transaction, IntegrityError
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.translation import gettext_lazy as _
+from djoser.serializers import PasswordRetypeSerializer
+from rest_framework import serializers, exceptions
+from rest_framework.exceptions import ValidationError
+from rest_framework_recursive.fields import RecursiveField
 from phonenumber_field.serializerfields import PhoneNumberField
 
+from djoser import utils
+
 from apps.users.models import User, Profile, Settings
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "email", "first_name", "last_name", "last_login"]
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -15,7 +17,107 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Profile
-        fields = "__all__"
+        fields = [
+            "id",
+            "phone_number",
+            "address",
+            "avatar",
+        ]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(required=False, allow_null=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "created_by",
+            "profile",
+        ]
+        read_only_fields = ("id",)
+
+
+class InviteUserMixin:
+    def create(self, validated_data):
+        try:
+            user = self.perform_create(validated_data)
+        except IntegrityError as e:
+            raise serializers.ValidationError(e)
+        return user
+
+    def perform_create(self, validated_data):
+        """
+        Invited users are created with is_active=False and no password.
+        User sets password on account activation.
+        """
+        user_account = validated_data.pop("user_accounts")
+        # TODO: ADD PROJECTS M2M RELATIONSHIP
+        # TODO: USE CELERY TO SEND EMAILS AND NOTIFICATIONS
+        with transaction.atomic():
+            user = User.objects.create_user(**validated_data)
+            
+        user.user_accounts.set(user_account)
+        
+        return user
+
+
+class InviteUserSerializer(InviteUserMixin, serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "created_by",
+            "user_accounts",
+        ]
+
+
+class UidAndTokenSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+
+    default_error_messages = {
+        "invalid_token": _("Invalid token for given user."),
+        "invalid_uid": _("Invalid user id or user doesn't exist."),
+    }
+
+    def validate(self, attrs):
+        validated_data = super().validate(attrs)
+
+        # uid validation have to be here, because validate_<field_name>
+        # doesn't work with modelserializer
+        try:
+            uid = utils.decode_uid(self.initial_data.get("uid", ""))
+            self.user = User.objects.get(pk=uid)
+        except (User.DoesNotExist, ValueError, TypeError, OverflowError):
+            key_error = "invalid_uid"
+            raise ValidationError(
+                {"uid": [self.error_messages[key_error]]}, code=key_error
+            )
+
+        is_token_valid = default_token_generator.check_token(
+            self.user, self.initial_data.get("token", "")
+        )
+        if is_token_valid:
+            return validated_data
+        else:
+            key_error = "invalid_token"
+            raise ValidationError(
+                {"token": [self.error_messages[key_error]]}, code=key_error
+            )
+
+
+
+class InviteUserActivationSerializer(UidAndTokenSerializer, PasswordRetypeSerializer):
+    """Invited users need to activate their account and set password."""
+
+    pass
 
 
 class SettingSerializer(serializers.ModelSerializer):
--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -1,23 +1,111 @@
-from rest_framework import status
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import status  # noqa: F401
+from rest_framework.decorators import action  # noqa: F401
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response  # noqa: F401
 from rest_framework.viewsets import ModelViewSet
+from djoser.compat import get_user_email
 
-from .serializers import UserSerializer, ProfileSerializer, SettingSerializer
+
+from .serializers import (
+    UserSerializer,
+    InviteUserSerializer,
+    InviteUserActivationSerializer,
+    ProfileSerializer,
+    SettingSerializer,
+)
 from apps.users.models import User, Profile, Settings
+from apps.users.email import InvitationEmail, ConfirmationEmail
 
 
-# TODO: ADD permisson to check if isadmin or isself on change password action
 class UserViewset(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
+    def get_queryset(self):
+        user = self.get_instance()
+        queryset = super().get_queryset()
+        # TODO: JUST RETURN USERS OF THE SAME ACCOUNT AND OR COLLABORATION GROUP
+        return queryset
 
-class ProfileViewset(ModelViewSet):
+    def get_permissions(self):
+        """remove require auth permission on invited user activation view."""
+        if self.action == "activation":
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
+
+    def get_instance(self):
+        return self.request.user
+
+    def perform_create(self, serializer):
+        user = serializer.save(created_by=self.request.user)
+       
+    def get_serializer(self, *args, **kwargs):
+        if isinstance(kwargs.get("data", {}), list):
+            kwargs["many"] = True
+        return super().get_serializer(*args, **kwargs)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return UserSerializer
+        elif self.action == "invitation":
+            return InviteUserSerializer
+        elif self.action == "activation":
+            return InviteUserActivationSerializer
+        elif self.action == "resend_invitation":
+            raise NotImplementedError("This Functionality has not been implimented")
+        return self.serializer_class
+
+    @action(methods=["POST"], detail=False)
+    def invitation(self, request, *args, **kwargs):
+        serializer = self.get_serializer(
+            data=request.data, context={"request": request}
+        )
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user = serializer.save(created_by=request.user)
+
+        # send email to user
+        context = {"user": user, "from_user": request.user.email}
+        to = [get_user_email(user)]
+        try:
+            InvitationEmail(self.request, context).send(to)
+        except (Exception) as e:
+            print(str(e))
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(methods=["POST"], detail=True)
+    def resend_invitation(self, request, id=None, *args, **kwargs):
+        # TODO: IN FRONTEND FOR USER HAVE LIST ON INVITATIONS AND OPTION TO RESEND
+        raise NotImplementedError("This method has not been implimented")
+
+    @action(methods=["POST"], detail=False)
+    def activation(self, request, *args, **kwargs):
+        serializer = self.get_serializer(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.user
+        user.set_password(serializer.data["new_password"])
+        user.is_active = True
+        user.save()
+        # TODO: SEND EMAIL TO USER AND NOTIFICATION TO INVITEE
+        
+        context = {"user": user}
+        to = [get_user_email(user)]
+        try:
+            ConfirmationEmail(self.request, context).send(to)
+        except Exception as e:
+            print(str(e))
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserProfileViewset(ModelViewSet):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
 
 
-class SettingsViewset(ModelViewSet):
+class UserSettingsViewset(ModelViewSet):
     serializer_class = SettingSerializer
     queryset = Settings.objects.all()
